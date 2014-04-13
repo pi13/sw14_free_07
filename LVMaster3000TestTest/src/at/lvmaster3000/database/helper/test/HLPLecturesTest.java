f86d222d2fe96dd025c302b56833ac43fee1f805
Doris Pedratscher <doris.pedratscher@student.tugraz.at>
2014-04-13 11:16:56 +0200

    set up test project

---
+++ LVMaster3000TestTest/src/at/lvmaster3000/database/helper/test/HLPLecturesTest.java
@@ -0,0 +1,10 @@
+package at.lvmaster3000.database.helper.test;
+
+import android.test.AndroidTestCase;
+
+public class HLPLecturesTest extends AndroidTestCase{
+
+	public void testLectureTable(){
+		
+	}
+}
