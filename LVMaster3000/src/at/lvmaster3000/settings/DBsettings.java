3a4746a2ed2b14835d80fa444e998e01b8199106
Rene Derler <derleredv@gmail.com>
2014-04-13 13:54:43 +0200

    relations helper added

--- LVMaster3000/src/at/lvmaster3000/settings/DBsettings.java
+++ LVMaster3000/src/at/lvmaster3000/settings/DBsettings.java
@@ -8,6 +8,7 @@ public class DBsettings {
 	public static final String LOG_TAG_EXAMS  = "LVM3K_SQL_EXAMS";
 	public static final String LOG_TAG_DATES  = "LVM3K_SQL_DATES";
 	public static final String LOG_TAG_COWORKERS  = "LVM3K_SQL_COWORKERS";
+	public static final String LOG_TAG_RELATIONS  = "LVM3K_SQL_RELATIONS";
 	public static final String DATABASE_NAME = "lvm3k.db";
 	public static final int DATABASE_VERSION = 1;
 }
