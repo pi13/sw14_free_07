332bdcd551bc1ca7426436f8978f7ab35870d444
Rene Derler <derleredv@gmail.com>
2014-04-13 12:58:25 +0200

    objects + lists for coworkers and dates added

---
+++ LVMaster3000/src/at/lvmaster3000/database/objects/Date.java
@@ -0,0 +1,64 @@
+package at.lvmaster3000.database.objects;
+
+import android.database.Cursor;
+import android.util.Log;
+import at.lvmaster3000.settings.DBsettings;
+
+public class Date {
+	private long id;
+	private long timestamp;
+	private String location;
+	private String type;
+	private String comment;
+	
+	public long getID() {
+		return id;
+	}
+	public void setID(long id) {
+		this.id = id;
+	}
+	public long getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+	public Date cursorToDate(Cursor cursor) {
+		//COL_ID, COL_TIMESTAMP, COL_LOCATION, COL_TYPE, COL_COMMENT
+		this.id = cursor.getLong(0);
+		this.timestamp = cursor.getLong(1);
+		this.location = cursor.getString(2);
+		this.type = cursor.getString(3);
+		this.comment = cursor.getString(4);
+		
+		return this;
+	}
+	
+	public void printDate() {
+		String msg = "ID: " + this.id;
+		msg += " | Timestamp: " + this.timestamp;
+		msg += " | Location: " + this.location;
+		msg += " | Type: " + this.type;
+		msg += " | Comment" + this.comment;
+		Log.d(DBsettings.LOG_TAG_DATES, msg);		
+	}
+}
