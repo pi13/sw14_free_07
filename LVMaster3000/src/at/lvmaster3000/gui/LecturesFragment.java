954396a72957a6832e356fe855bea31f7348817b
Amra Dzombic <thepi13@gmail.com>
2014-05-10 19:19:51 +0200

    UI List: list item click handling

--- LVMaster3000/src/at/lvmaster3000/gui/LecturesFragment.java
+++ LVMaster3000/src/at/lvmaster3000/gui/LecturesFragment.java
@@ -1,30 +1,53 @@
 package at.lvmaster3000.gui;
 
 
+import android.app.AlertDialog;
+import android.app.Fragment;
+import android.app.LauncherActivity.ListItem;
 import android.app.ListFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import at.lvmaster3000.R;
 
-public class LecturesFragment extends ListFragment{
+public class LecturesFragment extends Fragment implements OnItemClickListener{
 	
 	public LecturesFragment() {
 		// TODO Auto-generated constructor stub
 	}
 	
-	  @Override
-	  public void onActivityCreated(Bundle savedInstanceState) {
-	    super.onActivityCreated(savedInstanceState);
-	    String[] values = getResources().getStringArray(R.array.dummy_items);
-	    ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
-	        R.layout.single_list_item, R.id.label, values);
-	    setListAdapter(adapter);
-	  }
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+ 
+        final ListView list = (ListView) inflater.inflate(R.layout.fragment_list_layout, container, false);
+        attachAdapter(list);
+        list.setOnItemClickListener(this);
+        return list;
+    }
 
-	  @Override
-	  public void onListItemClick(ListView l, View v, int position, long id) {
-	    // do something with the data
-	  }
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id) 
+	{
+		AlertDialog.Builder dlgAlert = new AlertDialog.Builder(parent.getContext());
+		String label = (String)parent.getItemAtPosition(position);
+		dlgAlert.setTitle(label);
+		dlgAlert.setMessage(label + " has been clicked!");
+		dlgAlert.setPositiveButton("OK", null);
+	    dlgAlert.show();
+	}
+	
+	private void attachAdapter(ListView list)
+	{
+		ArrayAdapter<String> items = new ArrayAdapter<String>(list.getContext().getApplicationContext(), 
+														      R.layout.single_list_item, R.id.list_item_label, 
+														      getResources().getStringArray(R.array.dummy_items));
+		list.setAdapter(items);
+	}
 }
